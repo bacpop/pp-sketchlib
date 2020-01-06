b77266298eac3da6a70187ecc83196753bc52e6a
John <lees.john6@gmail.com>
2020-01-06 13:46:47 +0000

    Use string_view lite instead of stdlib
    Also unify makefiles

--- src/seqio.hpp
+++ src/seqio.hpp
@@ -9,8 +9,8 @@
 // C/C++/C++11/C++17 headers
 #include <cstddef>
 #include <string>
-#include <string_view> // O(1) substr operations
 #include <vector>
+#include <nonstd/string_view.hpp> // O(1) substr operations
 
 #include <zlib.h>
 #include <stdio.h>
@@ -22,8 +22,8 @@ class SeqBuf
     public:
         SeqBuf(const std::vector<std::string>& filenames, const size_t kmer_len);
 
-	    std::string_view get_fwd(const size_t kmer_len) const { return seq_view.substr(current_base, kmer_len); }
-	    std::string_view get_rev(const size_t kmer_len) const { return rcseq_view.substr(seq_view.size() - kmer_len - current_base, kmer_len); }
+	    nonstd::string_view get_fwd(const size_t kmer_len) const { return seq_view.substr(current_base, kmer_len); }
+	    nonstd::string_view get_rev(const size_t kmer_len) const { return rcseq_view.substr(seq_view.size() - kmer_len - current_base, kmer_len); }
 	    size_t nseqs() const { return sequence.size(); }
         bool eof() const { return _end; }
         bool is_reads() const { return _reads; }
@@ -35,8 +35,8 @@ class SeqBuf
         std::vector<std::string> sequence;
         std::vector<std::string> rc_sequence;
 
-        std::string_view seq_view;
-        std::string_view rcseq_view;
+        nonstd::string_view seq_view;
+        nonstd::string_view rcseq_view;
         size_t current_seq;
         size_t current_base;
 
