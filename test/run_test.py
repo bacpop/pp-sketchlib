cbd21fd69c03629ba5e25652a746638d4aa319d4
John <lees.john6@gmail.com>
2020-01-21 17:50:26 +0000

    100% certain this will get the tests to pass
    No chance of more commits like this

--- test/run_test.py
+++ test/run_test.py
@@ -28,7 +28,7 @@ sys.stderr.write("Testing sketching via python\n")
 subprocess.run("poppunk_sketch --sketch --rfile references.txt --ref-db test_db --sketch-size 10000 --min-k 15 --k-step 4", shell=True, check=True)
 # calculate distances
 sys.stderr.write("Testing distances via python\n")
-subprocess.run("poppunk_sketch --query --ref-db test_db --query-db test_db", shell=True, check=True)
+subprocess.run("poppunk_sketch --query --ref-db test_db --query-db test_db --min-k 15 --k-step 4", shell=True, check=True)
 
 # C++ test
 if not args.no_cpp:
