b07f5245805ac0f721d02df2da51d97de0f7c839
John Lees <lees.john6@gmail.com>
2020-05-12 13:57:40 +0100

    Correct order of longToSquareMulti test

--- test/test-matrix.py
+++ test/test-matrix.py
@@ -9,7 +9,7 @@ def check_res(res, expected):
 
 rr_mat = np.array([1, 2, 3, 4, 5, 6], dtype=np.float32)
 qq_mat = np.array([8], dtype=np.float32)
-qr_mat = np.array([0.5, 1, 0.5, 1, 0.5, 1, 0.5, 1], dtype=np.float32)
+qr_mat = np.array([10, 20, 10, 20, 10, 20, 10, 20], dtype=np.float32)
 
 square1 = pp_sketchlib.longToSquare(rr_mat, 2)
 square2 = pp_sketchlib.longToSquareMulti(rr_mat, qr_mat, qq_mat)
@@ -20,12 +20,12 @@ square1_res = np.array([[0, 1, 2, 3],
                         [3, 5, 6, 0]], dtype=np.float32)
 
 
-square2_res = np.array([[0, 1, 2, 3, 0.5, 1], 
-                        [1, 0, 4, 5, 0.5, 1], 
-                        [2, 4, 0, 6, 0.5, 1], 
-                        [3, 5, 6, 0, 0.5, 1],
-                        [0.5, 1, 0.5, 1, 0, 8],
-                        [1, 0.5, 1, 0.5, 8, 0]], dtype=np.float32)
+square2_res = np.array([[0, 1, 2, 3, 10, 20], 
+                        [1, 0, 4, 5, 10, 20], 
+                        [2, 4, 0, 6, 10, 20], 
+                        [3, 5, 6, 0, 10, 20],
+                        [10, 10, 10, 10, 0, 8],
+                        [20, 20, 20, 20, 8, 0]], dtype=np.float32)
 
 check_res(square1_res, square1)
 check_res(square2_res, square2)
