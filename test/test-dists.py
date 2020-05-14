753735bd5960601ceaff21f71c4c7754097ebaac
John Lees <lees.john6@gmail.com>
2020-05-14 14:41:42 +0100

    Test jaccard distances too

--- test/test-dists.py
+++ test/test-dists.py
@@ -6,7 +6,7 @@ import pickle
 
 ref_db = "test_db"
 old_results = "ppsketch_ref"
-max_diff = 0.01
+max_diff = 0.02
 
 def iterDistRows(refSeqs, querySeqs, self=True):
     if self:
@@ -20,12 +20,6 @@ def iterDistRows(refSeqs, querySeqs, self=True):
             for ref in refSeqs:
                 yield(ref, query)
 
-def check_res(res, expected):
-    if (not np.all(res == expected)):
-        print(res)
-        print(expected)
-        raise RuntimeError("Results don't match")
-
 # Generate distances
 rList = []
 ref = h5py.File(ref_db + ".h5", 'r')
@@ -35,17 +29,21 @@ for sample_name in list(ref['sketches'].keys()):
 db_kmers = ref['sketches/' + rList[0]].attrs['kmers']
 
 distMat = pp_sketchlib.queryDatabase(ref_db, ref_db, rList, rList, db_kmers)
+jaccard_dists = pp_sketchlib.queryDatabase(ref_db, ref_db, rList, rList, db_kmers, jaccard = True) 
+distMat = np.hstack((distMat, jaccard_dists))
 
 # Read old distances
 with open(old_results + ".pkl", 'rb') as pickle_file:
     rList_old, qList_old, self = pickle.load(pickle_file)
 oldDistMat = np.load(old_results + ".npy")
+oldJaccardDistMat = np.load(old_results + "_jaccard.npy")
+oldDistMat = np.hstack((oldDistMat, oldJaccardDistMat))
 
 # Check both match
 assert(rList_old == rList)
 names = iterDistRows(rList, rList, True)
 for i, (ref, query) in enumerate(names):
-    for j, (dist) in enumerate(['core', 'accessory']):
+    for j, (dist) in enumerate(['core', 'accessory'] + db_kmers):
         if oldDistMat[i, j] != 0 and distMat[i, j] != 0:
             diff = 2*(distMat[i, j] - oldDistMat[i, j])/(oldDistMat[i, j] + distMat[i, j]) 
             if (abs(diff) > max_diff):
