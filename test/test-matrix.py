7c42af2b7fc9989466b7bc2f0bdcc162517486f2
John Lees <lees.john6@gmail.com>
2020-05-17 15:40:16 +0100

    Add test for assignBoundary

--- test/test-matrix.py
+++ test/test-matrix.py
@@ -1,5 +1,25 @@
+import os, sys
+
+sys.path.insert(0, '../build/lib.linux-x86_64-3.7')
 import pp_sketchlib
 import numpy as np
+
+# Original PopPUNK function
+def withinBoundary(dists, x_max, y_max, slope=2):
+    boundary_test = np.ones((dists.shape[0]))
+    for row in range(boundary_test.size):
+        if slope == 2:
+            in_tri = dists[row, 0]*dists[row, 1] - (x_max-dists[row, 0])*(y_max-dists[row, 1])
+        elif slope == 0:
+            in_tri = dists[row, 0] - x_max
+        elif slope == 1:
+            in_tri = dists[row, 1] - y_max
+
+        if in_tri < 0:
+            boundary_test[row] = -1
+        elif in_tri == 0:
+            boundary_test[row] = 0
+    return(boundary_test)
 
 def check_res(res, expected):
     if (not np.all(res == expected)):
@@ -36,4 +56,14 @@ x = np.arange(0, 1, 0.1, dtype=np.float32)
 y = np.arange(0, 1, 0.1, dtype=np.float32)
 xv, yv = np.meshgrid(x, y)
 distMat = np.hstack((xv.reshape(-1,1), yv.reshape(-1,1)))
-assign1 = pp_sketchlib.assignThreshold(distMat, 0, 0.5, 0, 2)
+assign0 = pp_sketchlib.assignThreshold(distMat, 0, 0.5, 0.5, 2)
+assign1 = pp_sketchlib.assignThreshold(distMat, 1, 0.5, 0.5, 2)
+assign2 = pp_sketchlib.assignThreshold(distMat, 2, 0.5, 0.5, 2)
+
+assign0_res = withinBoundary(distMat, 0.5, 0.5, 0) 
+assign1_res = withinBoundary(distMat, 0.5, 0.5, 1) 
+assign2_res = withinBoundary(distMat, 0.5, 0.5, 2)
+
+check_res(assign0, assign0_res)
+check_res(assign1, assign1_res)
+check_res(assign2, assign2_res)
