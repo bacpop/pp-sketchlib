6b5c964ab7780fda63c4b5d502f50af50f22e344
John Lees <lees.john6@gmail.com>
2020-08-10 12:31:58 +0100

    Update read_test for GPU

--- src/read_test.cpp
+++ src/read_test.cpp
@@ -6,6 +6,7 @@
 #include "database.hpp"
 #include "random_match.hpp"
 #include "api.hpp"
+#include "gpu.hpp"
 #include "countmin.hpp"
 #include "seqio.hpp"
 
@@ -14,10 +15,10 @@ int main (int argc, char* argv[])
     // Runs a test of functionality
     
     std::vector<size_t> kmer_lengths {15, 29};
-    SeqBuf ref_seq({argv[2]}, kmer_lengths.back());
+    SeqBuf ref_seq({argv[2], argv[3]}, kmer_lengths.back());
     Reference ref(argv[1], ref_seq, kmer_lengths, 156, true, 20, false);
-    SeqBuf query_seq({argv[4], argv[5]}, kmer_lengths.back());
-    Reference query(argv[3], query_seq, kmer_lengths, 156, true, 20, false);
+    SeqBuf query_seq({argv[5], argv[6]}, kmer_lengths.back());
+    Reference query(argv[4], query_seq, kmer_lengths, 156, true, 20, false);
 
     RandomMC random(true);
     std::cout << ref.jaccard_dist(query, 15, random) << std::endl;
@@ -25,6 +26,16 @@ int main (int argc, char* argv[])
 
     auto core_acc = ref.core_acc_dist<RandomMC>(query, random); 
     std::cout << std::get<0>(core_acc) << "\t" << std::get<1>(core_acc) << std::endl;
+
+    create_sketches_cuda("gpu_db",
+                   {argv[1], argv[4]},
+                   {{argv[2], argv[3]}, {argv[5], argv[6]}},
+                   kmer_lengths,
+                   156,
+                   true,
+                   20,
+				   4,
+                   0);
 
     return 0;
 }
