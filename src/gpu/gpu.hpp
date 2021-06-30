324efbc3c6302a50e0bafd21fd3d3a9176b2778d
John Lees <lees.john6@gmail.com>
2021-06-30 09:56:37 +0100

    Split gpu countmin into header/functions

--- src/gpu/gpu.hpp
+++ src/gpu/gpu.hpp
@@ -11,6 +11,7 @@
 #include <memory>
 #include <vector>
 
+#include "gpu_countmin.cuh"
 #include "reference.hpp"
 
 static const int warp_size = 32;
--- src/gpu/gpu_countmin.cuh
+++ src/gpu/gpu_countmin.cuh
@@ -12,23 +12,13 @@ constexpr uint64_t table_cells = table_rows * table_width;
 
 class GPUCountMin {
 public:
-  GPUCountMin()
-    : _table_width_bits(table_width_bits),
-      _table_width(table_width), _hash_per_hash(hash_per_hash),
-      _table_rows(table_rows), _table_cells(table_cells) {
-    CUDA_CALL(cudaMalloc((void **)&_d_countmin_table,
-                        table_cells * sizeof(unsigned int)));
-    reset();
-  }
+  GPUCountMin();
 
-  ~GPUCountMin() { CUDA_CALL(cudaFree(_d_countmin_table)); }
+  ~GPUCountMin();
 
   unsigned int *get_table() { return _d_countmin_table; }
 
-  void reset() {
-    CUDA_CALL(
-      cudaMemset(_d_countmin_table, 0, table_cells * sizeof(unsigned int)));
-  }
+  void reset();
 
 private:
   // delete move and copy to avoid accidentally using them
@@ -44,33 +34,4 @@ private:
   const uint64_t _table_cells;
 };
 
-// Create a new hash from an nthash
-__device__ inline uint64_t shifthash(const uint64_t hVal, const unsigned k,
-  const unsigned i) {
-  uint64_t tVal = hVal * (i ^ k * multiSeed);
-  tVal ^= tVal >> multiShift;
-  return (tVal);
-}
 
-// Loop variables are global constants defined in gpu.hpp
-__device__ unsigned int add_count_min(unsigned int *d_countmin_table,
-                                      uint64_t hash_val, const int k) {
-  unsigned int min_count = UINT32_MAX;
-  for (int hash_nr = 0; hash_nr < table_rows; hash_nr += hash_per_hash) {
-    uint64_t current_hash = hash_val;
-    for (uint i = 0; i < hash_per_hash; i++) {
-      uint32_t hash_val_masked = current_hash & table_width;
-      unsigned int cell_count =
-          atomicInc(d_countmin_table + (hash_nr + i) * table_width +
-                        hash_val_masked,
-                    UINT32_MAX) +
-          1;
-      if (cell_count < min_count) {
-        min_count = cell_count;
-      }
-      current_hash = current_hash >> table_width_bits;
-    }
-    hash_val = shifthash(hash_val, k, hash_nr / 2);
-  }
-  return (min_count);
-}
