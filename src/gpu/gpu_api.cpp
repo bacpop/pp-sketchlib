ea7a9cfc6d7d0a094e515fa05a959f64d58f232b
John Lees <lees.john6@gmail.com>
2021-11-19 15:45:23 +0000

    Combine setup for cuda dists

--- src/gpu/gpu_api.cpp
+++ src/gpu/gpu_api.cpp
@@ -303,6 +303,93 @@ flatten_by_samples(const std::vector<Reference> &sketches,
   return flat_ref;
 }
 
+// Small struct used in cuda_dists_init
+struct dist_params {
+  bool self;
+  SketchStides ref_strides;
+  SketchStrides query_strides;
+  long long dist_rows;
+  long n_samples;
+  unsigned int chunks;
+  size_t calc_per_chunk;
+  unsigned int num_big_chunks;
+  unsigned int total_chunks;
+};
+
+// Helper function which checks device memory size and whether chunking
+// of input is necessary
+dist_params cuda_dists_init(const std::vector<Reference> &ref_sketches,
+                     const std::vector<Reference> &query_sketches,
+                     const std::vector<size_t> &kmer_lengths,
+                     const int device_id) {
+  dist_params params;
+
+  size_t mem_free, mem_total, shared_size;
+  std::tie(mem_free, mem_total, shared_size) = initialise_device(device_id);
+  std::cerr << "Calculating distances on GPU device " << device_id << std::endl;
+
+  // Check sketches are compatible
+  params.self = false;
+  size_t bbits = ref_sketches[0].bbits();
+  size_t sketchsize64 = ref_sketches[0].sketchsize64();
+  checkSketchParamsMatch(ref_sketches, kmer_lengths, bbits, sketchsize64);
+
+  // Set up sketch information and sizes
+  params.ref_strides.bbits = bbits;
+  params.ref_strides.sketchsize64 = sketchsize64;
+  params.query_strides = params.ref_strides;
+
+  if (ref_sketches == query_sketches) {
+    params.self = true;
+    params.dist_rows = static_cast<long long>(0.5 * (ref_sketches.size()) *
+                                       (ref_sketches.size() - 1));
+    params.n_samples = ref_sketches.size();
+  } else {
+    // Also check query sketches are compatible
+    checkSketchParamsMatch(query_sketches, kmer_lengths, bbits, sketchsize64);
+    params.dist_rows = ref_sketches.size() * query_sketches.size();
+    params.n_samples = ref_sketches.size() + query_sketches.size();
+  }
+  double est_size =
+      (bbits * sketchsize64 * kmer_lengths.size() * params.n_samples *
+           sizeof(uint64_t) + // Size of sketches
+       kmer_lengths.size() * std::pow(random_match.n_clusters(), 2) *
+           sizeof(float) + // Size of random matches
+       params.n_samples * sizeof(uint16_t) +
+       params.dist_rows * 2 * sizeof(float)); // Size of distance matrix
+  std::cerr << "Estimated device memory required: " << std::fixed
+            << std::setprecision(0) << est_size / (1048576) << "Mb"
+            << std::endl;
+  std::cerr << "Total device memory: " << std::fixed << std::setprecision(0)
+            << mem_total / (1048576) << "Mb" << std::endl;
+  std::cerr << "Free device memory: " << std::fixed << std::setprecision(0)
+            << mem_free / (1048576) << "Mb" << std::endl;
+
+  params.chunks = 1;
+  if (est_size > mem_free * (1 - mem_epsilon) {
+    if (params.self) {
+      // To prevent memory being exceeded, total distance matrix is split up into
+      // chunks which do fit in memory. The most is needed in the 'corners' where
+      // two separate lots of sketches are loaded, hence the factor of two below
+
+      // These are iterated over in the same order as a square distance matrix.
+      // The i = j chunks are 'self', i < j can be skipped
+      // as they contain only lower triangle values, i > j work as query vs ref
+      if (est_size > mem_free * (1 - mem_epsilon)) {
+        params.chunks = floor((est_size * 2) / (mem_free * (1 - mem_epsilon))) + 1;
+      }
+      params.calc_per_chunk = params.n_samples / params.chunks;
+      params.num_big_chunks = params.n_samples % params.chunks;
+      params.total_chunks = (params.chunks * (params.chunks + 1)) >> 1;
+    } else {
+      throw std::runtime_error(
+          "Using greater than device memory is unsupported for query mode. "
+          "Split your input into smaller chunks");
+    }
+  }
+  return params;
+}
+
 // Main function callable via API
 // Checks inputs
 // Flattens sketches
@@ -314,55 +401,8 @@ NumpyMatrix query_db_cuda(std::vector<Reference> &ref_sketches,
                           const std::vector<size_t> &kmer_lengths,
                           RandomMC &random_match, const int device_id,
                           const unsigned int num_cpu_threads) {
-  size_t mem_free, mem_total, shared_size;
-  std::tie(mem_free, mem_total, shared_size) = initialise_device(device_id);
-  std::cerr << "Calculating distances on GPU device " << device_id << std::endl;
-
-  // Check sketches are compatible
-  bool self = false;
-  size_t bbits = ref_sketches[0].bbits();
-  size_t sketchsize64 = ref_sketches[0].sketchsize64();
-  checkSketchParamsMatch(ref_sketches, kmer_lengths, bbits, sketchsize64);
-
-  // Set up sketch information and sizes
-  SketchStrides ref_strides;
-  ref_strides.bbits = bbits;
-  ref_strides.sketchsize64 = sketchsize64;
-  SketchStrides query_strides = ref_strides;
-
-  long long dist_rows;
-  long n_samples = 0;
-  if (ref_sketches == query_sketches) {
-    self = true;
-    dist_rows = static_cast<long long>(0.5 * (ref_sketches.size()) *
-                                       (ref_sketches.size() - 1));
-    n_samples = ref_sketches.size();
-  } else {
-    // Also check query sketches are compatible
-    checkSketchParamsMatch(query_sketches, kmer_lengths, bbits, sketchsize64);
-    dist_rows = ref_sketches.size() * query_sketches.size();
-    n_samples = ref_sketches.size() + query_sketches.size();
-  }
-  double est_size =
-      (bbits * sketchsize64 * kmer_lengths.size() * n_samples *
-           sizeof(uint64_t) + // Size of sketches
-       kmer_lengths.size() * std::pow(random_match.n_clusters(), 2) *
-           sizeof(float) + // Size of random matches
-       n_samples * sizeof(uint16_t) +
-       dist_rows * 2 * sizeof(float)); // Size of distance matrix
-  std::cerr << "Estimated device memory required: " << std::fixed
-            << std::setprecision(0) << est_size / (1048576) << "Mb"
-            << std::endl;
-  std::cerr << "Total device memory: " << std::fixed << std::setprecision(0)
-            << mem_total / (1048576) << "Mb" << std::endl;
-  std::cerr << "Free device memory: " << std::fixed << std::setprecision(0)
-            << mem_free / (1048576) << "Mb" << std::endl;
-
-  if (est_size > mem_free * (1 - mem_epsilon) && !self) {
-    throw std::runtime_error(
-        "Using greater than device memory is unsupported for query mode. "
-        "Split your input into smaller chunks");
-  }
+  dist_params params =
+    cuda_dists_init(ref_sketches, query_sketches, kmer_lengths, device_id);
 
   // Turn the random matches into an array (same for any ref, query or subsample
   // thereof)
@@ -380,47 +420,32 @@ NumpyMatrix query_db_cuda(std::vector<Reference> &ref_sketches,
 
   // Ready to run dists on device
   SketchSlice sketch_subsample;
-  unsigned int chunks = 1;
   std::vector<float> dist_results(dist_rows * 2);
   NumpyMatrix coreSquare, accessorySquare;
-  if (self) {
-    // To prevent memory being exceeded, total distance matrix is split up into
-    // chunks which do fit in memory. The most is needed in the 'corners' where
-    // two separate lots of sketches are loaded, hence the factor of two below
-
-    // These are iterated over in the same order as a square distance matrix.
-    // The i = j chunks are 'self', i < j can be skipped
-    // as they contain only lower triangle values, i > j work as query vs ref
-    if (est_size > mem_free * (1 - mem_epsilon)) {
-      chunks = floor((est_size * 2) / (mem_free * (1 - mem_epsilon))) + 1;
-    }
-    size_t calc_per_chunk = n_samples / chunks;
-    unsigned int num_big_chunks = n_samples % chunks;
-
+  if (params.self) {
     // Only allocate these square matrices if they are needed
-    if (chunks > 1) {
-      coreSquare.resize(n_samples, n_samples);
-      accessorySquare.resize(n_samples, n_samples);
+    if (params.chunks > 1) {
+      coreSquare.resize(params.n_samples, params.n_samples);
+      accessorySquare.resize(params.n_samples, params.n_samples);
     }
-    unsigned int total_chunks = (chunks * (chunks + 1)) >> 1;
     unsigned int chunk_count = 0;
 
     sketch_subsample.query_offset = 0;
-    for (unsigned int chunk_i = 0; chunk_i < chunks; chunk_i++) {
-      sketch_subsample.query_size = calc_per_chunk;
-      if (chunk_i < num_big_chunks) {
+    for (unsigned int chunk_i = 0; chunk_i < params.chunks; chunk_i++) {
+      sketch_subsample.query_size = params.calc_per_chunk;
+      if (chunk_i < params.num_big_chunks) {
         sketch_subsample.query_size++;
       }
 
       sketch_subsample.ref_offset = sketch_subsample.query_offset;
-      for (unsigned int chunk_j = chunk_i; chunk_j < chunks; chunk_j++) {
+      for (unsigned int chunk_j = chunk_i; chunk_j < params.chunks; chunk_j++) {
         if (total_chunks > 1) {
           std::cerr << "Running chunk " << ++chunk_count << " of "
                     << total_chunks << std::endl;
         }
 
-        sketch_subsample.ref_size = calc_per_chunk;
-        if (chunk_j < num_big_chunks) {
+        sketch_subsample.ref_size = params.calc_per_chunk;
+        if (chunk_j < params.num_big_chunks) {
           sketch_subsample.ref_size++;
         }
 
@@ -430,7 +455,7 @@ NumpyMatrix query_db_cuda(std::vector<Reference> &ref_sketches,
             chunk_i == chunk_j, num_cpu_threads, shared_size);
 
         // Read intermediate dists out
-        if (chunks > 1) {
+        if (params.chunks > 1) {
           NumpyMatrix blockMat = Eigen::Map<
               Eigen::Matrix<float, Eigen::Dynamic, 2, Eigen::ColMajor>>(
               dist_results.data(), dist_results.size() / 2, 2);
@@ -461,7 +486,7 @@ NumpyMatrix query_db_cuda(std::vector<Reference> &ref_sketches,
   }
 
   NumpyMatrix dists_ret_matrix;
-  if (self && chunks > 1) {
+  if (params.self && params.chunks > 1) {
     // Chunked computation yields square matrix, which needs to be converted
     // back to long form
     Eigen::VectorXf core_dists = square_to_long(coreSquare, num_cpu_threads);
@@ -485,5 +510,6 @@ sparse_coo query_db_sparse(std::vector<Reference> &ref_sketches,
                           const int kNN, const size_t dist_col,
                           const int device_id,
                           const unsigned int num_cpu_threads) {
-  check_sparse_inputs(ref_sketches, kmer_lengths, random_chances, jaccard, dist_col);
+  dist_params params =
+    cuda_dists_init(ref_sketches, query_sketches, kmer_lengths, device_id);
 }
