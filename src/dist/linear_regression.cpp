64d874867e1fe9fcb23a956890c01b949248e86a
John Lees <lees.john6@gmail.com>
2022-11-17 15:53:12 +0000

    Fix kmer mat accesses

--- src/dist/linear_regression.cpp
+++ src/dist/linear_regression.cpp
@@ -29,11 +29,11 @@ std::tuple<float, float> fit_slope(const Eigen::MatrixXf &kmers,
         }
         Eigen::VectorXf dists_truncation = dists(Eigen::seqN(0, i));
         Eigen::VectorXf kmer_truncation = kmers(Eigen::seqN(0, i), Eigen::all);
-        slopes = (kmer_truncation.transpose() * kmer_truncation).ldlt().solve(kmer_truncation.transpose() * (dists_truncation.log()));
+        slopes = (kmer_truncation.transpose() * kmer_truncation).ldlt().solve(kmer_truncation.transpose() * (dists_truncation.array().log()));
         break;
       } else if (i == dists.size() - 1) {
         // See https://eigen.tuxfamily.org/dox/group__LeastSquares.html
-        slopes = (kmers.transpose() * kmers).ldlt().solve(kmers.transpose() * (dists.log()));
+        slopes = (kmers.transpose() * kmers).ldlt().solve(kmers.transpose() * (dists.array().log()));
       }
     }
 
@@ -60,7 +60,7 @@ std::tuple<float, float> regress_kmers(Reference *r1, Reference *r2,
   // Vector of points
   Eigen::VectorXf dists(kmers.size());
   for (unsigned int i = 0; i < kmers.size(); ++i) {
-    dists(i) = r1->jaccard_dist(*r2, kmers[i], random);
+    dists[i] = r1->jaccard_dist(*r2, kmers(i, 1), random);
   }
   return (fit_slope(kmers, dists, r1, r2));
 }
@@ -72,7 +72,7 @@ std::tuple<float, float> regress_kmers(Reference *r1, Reference *r2,
   // Vector of points
   Eigen::VectorXf dists(kmers.size());
   for (unsigned int i = 0; i < dists.size(); ++i) {
-    dists[i] = r1->jaccard_dist(*r2, kmers[i], random[i]);
+    dists[i] = r1->jaccard_dist(*r2, kmers(i, 1), random[i]);
   }
   return (fit_slope(kmers, dists, r1, r2));
 }
