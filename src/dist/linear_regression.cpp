8c364eea1f3f68bcc44ca5182657c75df0587466
John Lees <lees.john6@gmail.com>
2023-01-10 17:59:58 +0000

    More robust regression with zero distances (#88)
    * More general method for ignoring zeros in regression
    
    * Bump version
    
    * Fix the kmer slice
    
    * Support older eigen version
    
    * Eigen 3.3 compatible version
    
    * Size fix
    
    (fences vs posts)
    
    * Apparently robin hood needs limits header now?
    
    * Better regression validity check
    
    * Allow newer gcc versions
    
    * Update gcc options in the makefile too
    
    * Try using an older ubuntu image on azure

--- src/dist/linear_regression.cpp
+++ src/dist/linear_regression.cpp
@@ -22,19 +22,24 @@ std::tuple<float, float> fit_slope(const Eigen::MatrixXf &kmers,
   static const double tolerance = (5.0 / (r1->sketchsize64() * 64));
   try {
     Eigen::VectorXf slopes;
+    std::vector<int> valid;
     for (int i = 0; i < dists.size(); ++i) {
-      if (dists(i) < tolerance) {
-        if (i == 0) {
-          throw std::runtime_error("No non-zero Jaccard distances");
-        }
-        Eigen::VectorXf dists_truncation = dists.head(i);
-        Eigen::VectorXf kmer_truncation = kmers.topRows(i);
-        slopes = (kmer_truncation.transpose() * kmer_truncation).ldlt().solve(kmer_truncation.transpose() * (dists_truncation.array().log().matrix()));
-        break;
-      } else if (i == dists.size() - 1) {
-        // See https://eigen.tuxfamily.org/dox/group__LeastSquares.html
-        slopes = (kmers.transpose() * kmers).ldlt().solve(kmers.transpose() * (dists.array().log().matrix()));
+      if (dists(i) > tolerance) {
+        valid.push_back(i);
       }
+    }
+    size_t valid_size = valid.back() - valid.front() + 1;
+
+    if (__builtin_expect(valid_size < 2, 0)) {
+      throw std::runtime_error("No non-zero Jaccard distances");
+    } else if (__builtin_expect(valid_size == dists.size(), 1)) {
+      // See https://eigen.tuxfamily.org/dox/group__LeastSquares.html
+      slopes = (kmers.transpose() * kmers).ldlt().solve(kmers.transpose() * (dists.array().log().matrix()));
+    } else {
+      size_t slice_start = valid.front();
+      Eigen::VectorXf dists_truncation = dists.segment(slice_start, valid_size);
+      Eigen::MatrixXf kmer_truncation = kmers.block(slice_start, 0, valid_size, 2);
+      slopes = (kmer_truncation.transpose() * kmer_truncation).ldlt().solve(kmer_truncation.transpose() * (dists_truncation.array().log().matrix()));
     }
 
     if (slopes(1) < core_upper) {
