ad4766d449f339186a34e14189a2aff2ebc80cca
John Lees <lees.john6@gmail.com>
2023-12-18 17:03:13 +0000

    Revert changes in the wrapper

--- sketchlib-runner.py
+++ sketchlib-runner.py
@@ -3,9 +3,6 @@
 # Copyright 2019 John Lees
 
 """Wrapper for running python executable without installing"""
-import os
-os.environ["LD_PRELOAD"] = "/lib/x86_64-linux-gnu/libSegFault.so"
-
 
 from pp_sketch.__main__ import main
 
